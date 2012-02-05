320cd8b4802dc71c155e9b1381b8390501d76daa
Michael <fairchild@stimble.net>
2012-02-05 11:49:18 -0800

    Add express.js and Update with author code for authentication to github
    Still rough, but working.

--- app.js
+++ app.js
@@ -1,17 +1,44 @@
 // My SocketStream app
 
-var http = require('http')
-  , ss = require('socketstream');
+var http  = require('http')
+, ss      = require('socketstream')
+, authom  = require('authom')
+, express = require('express');
+
+var app = express.createServer();;
+
+var github = authom.createServer({
+  service: "github",
+  id: "6e084a8b6117d5cc16c3",
+  secret: "5a6f1b27aa4b70d2d8930e3cf43c8ecce4c04b32",
+  callback: "http://localhost:5000/users/auth/github/callback",
+  scope: ["gist"]
+})
+
+// called when a user is authenticated on any service
+authom.on("auth", function(req, res, data) {
+  console.log('Hit Auth!\n');
+  console.log(data);
+  console.log(req);
+  res.send(
+    "<html>" +
+      "<body>" +
+        "<div style='font: 300% sans-serif'>You are " + data.id + " on " + data.service + ".</div>" +
+        "<pre><code>" + JSON.stringify(data, null, 2) + "</code></pre>" +
+      "</body>" +
+    "</html>"
+  );
+})
 
 ss.client.define('chat', {
   view: 'chat.jade',
-  css:  ['libs', 'chat.styl'],
+  css:  ['chat.styl'],
   code: ['libs', 'modules', 'main']
 });
 
-ss.http.router.on('/chat', function(req, res) {
-  res.serve('chat');
-});
+// ss.http.router.on('/', function(req, res) {
+//   res.serve('chat');
+// });
 
 ss.session.store.use('redis', {redis: {host: 'localhost', port: 6379}});
 ss.publish.transport.use('redis', {redis: {host: 'localhost', port: 6379}});
@@ -29,7 +56,20 @@ ss.client.templateEngine.use(require('ss-hogan'));
 // Minimise and pack assets if you type  SS_ENV=production node app.js
 if (ss.env == 'production') ss.client.packAssets();
 
-var server = http.Server(ss.http.middleware);
-server.listen(3000);
+app.get('/login', function(req, res){
+  console.log('Hello there!\n')
+  res.send('<a href="/users/auth/github/callback">login via github</a>');
+});
 
+app.get("/users/auth/:service/callback", authom.app);
+
+
+app.get('/chat', function(req, res){
+  res.serve('chat');
+});
+
+app.stack = app.stack.concat(ss.http.middleware.stack)
+
+// var server = http.Server(ss.http.middleware);
+server = app.listen(5000);
 ss.start(server);
